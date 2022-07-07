5719970f0f5d75927269518f06f2832b2aad8711
Christimans <perezchristian3907@gmail.com>
2022-07-06 21:49:53 -0300

    agragado de DOM

--- proyecto.js
+++ proyecto.js
@@ -1,57 +1,81 @@
+let Bienvenido = document.getElementById("bienvenido")
+
 let nombre = prompt ("¿Cual es tu nombre?")
-alert ("Bienvenido " + nombre)
+Bienvenido.innerText = `Bienvenido ${nombre}`
 if(nombre = "Bienvenido"){
-    menu()
+
 
 }else{
     alert ("no se eligio ninguna opcion")
 }
+console.log(Bienvenido.innerText)
 
-function menu(){
-    let servicios = prompt ("Elige una opcion: \n 1. Contratacion \n 2. Presupuesto \n 3.Contacto \n 4. Salir")
+let navegador = document.getElementById("menu")
+contratacion()
+presupuesto()
 
-    switch (servicios){
-        case "1" :
-            contratacion()
-            menu()
-            break
-        case "2" :
-            presupuesto()
-            menu()
-            break
-        case "3" :
-            alert(
-                "Contactanos al: 03534-1568235 \n  Email: empresa@gmail.com" 
-            )
-            menu()
-            break
-        case "4":
-            alert("Gracias por usar nuestro servicios")
-            break
-        default:
-            alert("Opcion no valida")
-            menu()
-            break
-    }
-}
+let presu = document.getElementById("presu")
+
+
+
+let navPrincipal = `<div class="accordion-item">
+<h2 class="accordion-header" id="headingOne">
+  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne ">
+    Contratacion
+  </button>
+</h2>
+<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+  <div class="accordion-body">
+    <strong >${contrata} </strong> 
+  </div>
+</div>
+</div>
+<div class="accordion-item">
+<h2 class="accordion-header" id="headingTwo">
+  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo ">
+    Presupuesto
+  </button>
+</h2>
+<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+  <div class="accordion-body">
+    <strong>${dato}</strong>
+  </div>
+</div>
+</div>
+<div class="accordion-item">
+<h2 class="accordion-header" id="headingThree">
+  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+    Contacto
+  </button>
+</h2>
+<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+  <div class="accordion-body">
+    <strong><p>Contactanos al +54 3547 9642</p>
+    <p>email: aswefewq@hotmail.com</p></strong> 
+  </div>
+</div>
+</div> `
+
+navegador.innerHTML = navPrincipal 
+
+console.log(navegador.innerHTML)
+
+
+
+
 
 function contratacion(){
     let opcion = prompt("Opciones: \n 1. llave en mano \n 2. Solo mano de obra")
-    switch(opcion){
-        case "1":
-            alert ("Esta opcion incluye todo los servicios")
-            menu()
-            break
-        case "2":
-            alert ("Esta opcion solo incluye mano de obra. Él resto de los servicios puede ser opcional")
-            menu()
-            break;
-        default:
-            alert("saa")
-            menu()
-            break
+
+    if (opcion == "1"){
+        contrata = "Esta opcion incluye todo los servicios"
+    }else if (opcion == "2") {
+        contrata = "Esta opcion solo incluye mano de obra. Él resto de los servicios puede ser opcional"
+    }else {
+        contrata = "no selecionaste ninguna"
+    }
 }
-}
+
 
 function presupuesto() {
     
@@ -77,11 +101,10 @@ function presupuesto() {
 
             let agua = m3 * 1000
             MEDIDAS.push(agua)
-  
-            console.log(MEDIDAS)
-            alert("$" + resultado)
-            alert("Tu pileta tiene una capacidad de : " + agua + " litros")
             
+            dato = `Tu pileta tiene un monto de $${MEDIDAS[0]} y tendra un caudal de ${MEDIDAS[1]} litros`
+            
+            console.log(MEDIDAS)
     
         }else if (espacio == "2"){
             
@@ -89,21 +112,20 @@ function presupuesto() {
             
             if (casa == "1"){
                 
-                alert("El monoambiente solo contiene una habitacion, un baño y cocina")
-
+                dato = "El monoambiente solo contiene una habitacion, un baño y cocina"
 
             }else if(casa == "2"){
 
                 const sector =  []
 
                 const espacio = [
-                    {lugar: "Habitacion", precio: 35000},
-                    {lugar: "Baño", precio: 50000},
-                    {lugar: "Cocina", precio: 40000},
-                    {lugar: "Living", precio: 25000},
-                    {lugar: "Quincho", precio: 100000},
-                    {lugar: "Cochera", precio: 20000},
-                    {lugar: "Sala de juego", precio: 15000},
+                    {lugar: "Habitacion", precio: 350000},
+                    {lugar: "Baño", precio: 500000},
+                    {lugar: "Cocina", precio: 400000},
+                    {lugar: "Living", precio: 250000},
+                    {lugar: "Quincho", precio: 1000000},
+                    {lugar: "Cochera", precio: 200000},
+                    {lugar: "Sala de juego", precio: 150000},
                 ]
 
                 const habit = espacio.map (dato => dato.precio);
@@ -114,21 +136,26 @@ function presupuesto() {
                 function calculos (){
                     
                 let habitacion = parseInt(prompt(`Indique cuantas  ${hogar[0]}: `))
+                sector.push(habitacion)
 
                 let baños = parseInt(prompt(`Indique cuantas  ${hogar[1]}: `))
+                sector.push(baños)
                
                 let cocina = parseInt(prompt(`Indique cuantas  ${hogar[2]}: `))
+                sector.push(cocina)
                
                 let living = parseInt(prompt(`Indique cuantas  ${hogar[3]}: `))
+                sector.push(living)
                
                 let quincho = parseInt(prompt(`Indique cuantas  ${hogar[4]}: `))
+                sector.push(quincho)
                
                 let cochera = parseInt(prompt(`Indique cuantas  ${hogar[5]}: `))
+                sector.push(cochera)
               
                 let saladejuego = parseInt(prompt(`Indique cuantas  ${hogar[6]}: `))
-                
-                // let resto = prompt("Indica que otros espacio quiere ")
-                
+                sector.push(saladejuego)
+                               
 
                 let cH = habitacion * habit[0]
                 
@@ -157,7 +184,9 @@ function presupuesto() {
                 console.log(sector)
                 calculos()
 
-                alert(`Usted indico $${sector} `)
+                alert(`Usted indico $${sector[7]} `)
+
+                dato = `Tu Casa tiene un monto de $${sector[7]} la que contiene:  ${sector[0]} habitaciones  ${sector[1]} baños ${sector[2]} cocina ${sector[3]} living  ${sector[4]} quincho  ${sector[5]} cochera  ${sector[6]} sala de juego. `
 
                 let cp = confirm ("Desea aceptar")
                 //cs = casa personalizada
@@ -177,4 +206,5 @@ function presupuesto() {
     }
     
 }
+
 
